5325ce9554fc8a4546396d739feff51598048955
An Nguyen <an.nguyen@tuni.fi>
2024-11-02 13:07:29 +0200

    Fix login doesn't need role

--- src/functionalities/Login.js
+++ src/functionalities/Login.js
@@ -1,14 +1,14 @@
 import filter from "../filter";
 import eq from "../eq";
 
-export function producerLogin(email, password, role, credentials) {
+export const login = (email, password, credentials) => {
     if (!email || !password) {
         return { success: false, message: "Invalid credentials" };
     }
 
     const matchCredentials = filter(
         credentials,
-        (cred) => eq(cred.email, email) && eq(cred.password, password) && eq(cred.role, role)
+        (cred) => eq(cred.email, email) && eq(cred.password, password)
     );
 
     if (matchCredentials.length !== 0 && matchCredentials[0].length !== 0) {
